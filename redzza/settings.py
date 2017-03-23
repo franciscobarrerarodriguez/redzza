67c917f88f22c102173ce7250b59f2341e997645
larry852 <larryportocarrero@gmail.com>
2017-03-23 18:02:28 -0500

    Agregado templatesy urls basicas

--- redzza/settings.py
+++ redzza/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+ACCOUNT_ACTIVATION_DAYS = 7
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
     'profiles',
     'categories',
 ]
