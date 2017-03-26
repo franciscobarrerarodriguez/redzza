cfc7e45305d6f22b76888472bc0494bb93e522c3
franciscobarrerarodriguez <francisco.barrera@uptc.edu.co>
2017-03-26 02:02:19 -0500

    Ventanas para inicio de sesion

--- static/js/redzza/redzza.js
+++ static/js/redzza/redzza.js
@@ -1,8 +1,48 @@
+// https://developer.mozilla.org/en-US/docs/Web/API
 (function () {
-  document.getElementById('signup-button').addEventListener('click', function () {
-    console.log("modal registro");
+  /* form-login actions */
+  $('#form-login').submit(function (e) {
+    e.preventDefault();
+    var form = $(this);
+    $.ajax({
+      url: form.attr('data-url'),
+      data: form.serialize(),
+      dataType: 'json',
+      success: function (json) {
+        alert(json);/*Falta terminar*/
+      }
+    });
   });
-  document.getElementById('login-button').addEventListener('click', function () {
-    console.log("modal login");
+
+  /* form-signup actions */
+  $('#form-signup').submit(function (e) {
+    e.preventDefault();
+    var form = $(this);
+    $.ajax({
+      url: form.attr('data-url'),
+      data: form.serialize(),
+      dataType: 'json',
+      success: function (json) {
+        alert(json);/*Falta terminar*/
+      }
+    });
   });
+
 })();
+
+/* === Modal actions
+* Display de modal by id */
+function getModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+/* Hidde modal by id */
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
