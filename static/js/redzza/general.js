d01de30582b8108bced1b237100fe3dc61f287b6
franciscobarrerarodriguez <francisco.barrera@uptc.edu.co>
2017-05-16 17:46:54 -0500

    Falta metodo post para settings

--- static/js/redzza/general.js
+++ static/js/redzza/general.js
@@ -9,17 +9,29 @@ $(document).on("ready", function () {
   $('.parallax').parallax();
   /* Init tabs */
   $('ul.tabs').tabs({'swipeable':'true'});
+  /* Init date picker
+  https://www.theinvestmentassociation.org/assets/components/ima_circularwizard/js/pickadate/docs.htm#formats
+  */
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    format: 'mmm/dd/yyyy'
+  });
   /* Init modals */
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 
   /* Update email form user */
-  $('#new-email').click(function(e) {
+  $('#new-email').submit(function (e) {
     e.preventDefault();
     var email1 = $("input[id='email-1']");
     var email2 = $("input[id='email-2']");
     if (email1.val() == email2.val()) {
-      console.log("iguales");
+      var response = ajaxPostCall($(this).attr('data-url'), $(this).serialize());
+      if (response.success == true) {
+        $('#modal1').modal('close');
+      }
+      Materialize.toast(response.msg, 4000) // 4000 is the duration of the toast
     }else {
       $(email1).addClass("invalid");
       $(email2).addClass("invalid");
@@ -28,19 +40,28 @@ $(document).on("ready", function () {
   });
 
   /* Update user */
-  $('#update-user').click(function (e) {
+  $('#update-user').submit(function (e) {
     e.preventDefault();
-    var newUser = $("input[id='new-username']");//falta val
-    console.log("Cambiar user");
+    // var newUser = $("input[id='new-username']");//falta val
+    var response = ajaxPostCall($(this).attr('data-url'), $(this).serialize());
+    if (response.success == true) {
+      $('#modal2').modal('close');
+    }
+    Materialize.toast(response.msg, 4000) // 4000 is the duration of the toast
   });
 
   /* Update user's password */
-  $('#update-password').click(function (e) {
+  $('#update-password').submit(function (e) {
     e.preventDefault();
     var password1 = $("input[id='new-password']");
     var password2 = $("input[id='new-password-2']");
     if (password1.val() == password2.val()) {
-      console.log("son iguales");
+      var response = ajaxPostCall($(this).attr('data-url'), $(this).serialize());
+      if (response.success == true) {
+        $('#modal3').modal('close');
+      }
+      Materialize.toast(response.msg, 4000) // 4000 is the duration of the toast
+
     }else {
       $(password1).addClass("invalid");
       $(password2).addClass("invalid");
@@ -49,21 +70,33 @@ $(document).on("ready", function () {
   });
 
   /* Update user's phone */
-  $('#update-phone').click(function (e) {
+  $('#update-phone').submit(function (e) {
     e.preventDefault();
-    var newPhone = $("input[id='new-phone']");//falta val
-    console.log('update phone');
+    // var newPhone = $("input[id='new-phone']");//falta val
+    var response = ajaxPostCall($(this).attr('data-url'), $(this).serialize());
+    if (response.success == true) {
+      $('#modal5').modal('close');
+    }
+    Materialize.toast(response.msg, 4000) // 4000 is the duration of the toast
   });
 
   /* Update user's address */
-  $('#update-address').click(function (e) {
+  $('#update-address').submit(function (e) {
     e.preventDefault();
-    var newAddress = $("input[id='new-address']");
-    console.log("Update address");
+    // var newAddress = $("input[id='new-address']");//Direccion o ubicacion preguntar a Larry?
+    var response = ajaxPostCall($(this).attr('data-url'), $(this).serialize());
+    if (response.success == true) {
+      $('#modal6').modal('close');
+    }
+    Materialize.toast(response.msg, 4000) // 4000 is the duration of the toast
   });
 
 });
 
-function ajaxGetPostCall(dataUrl, data) {
-
+function ajaxPostCall(dataUrl, data) {
+  var json;
+  $.post(dataUrl, data, function (response) {
+    json = response;
+  });
+  return json;
 }
