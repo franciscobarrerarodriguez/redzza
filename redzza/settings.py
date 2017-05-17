2a75dee4912f76973b3e924987ed55054499b107
franciscobarrerarodriguez <francisco.barrera@uptc.edu.co>
2017-05-16 21:02:59 -0500

    Heroku

--- redzza/settings.py
+++ redzza/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files directories
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
