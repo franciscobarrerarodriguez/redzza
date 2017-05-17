7b502dd27eac7ce3e4ea56d4e11c233fa9218acf
larry852 <larryportocarrero@gmail.com>
2017-05-16 20:28:04 -0500

    Configuracion para heroku

--- redzza/settings.py
+++ redzza/settings.py
@@ -17,7 +17,6 @@ import os
 import dj_database_url
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -139,14 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 # Static files directories
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 # Docs, pictures and more
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
