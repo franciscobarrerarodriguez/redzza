0accca10d52977244ed068aabe79cc485b0f36e0
larry852 <larryportocarrero@gmail.com>
2017-07-18 17:20:28 -0500

    Eliminacion de backend de autenticacion de facebook

--- redzza/settings.py
+++ redzza/settings.py
@@ -157,7 +157,7 @@ MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.facebook.FacebookOAuth2',
+    # 'social_core.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
