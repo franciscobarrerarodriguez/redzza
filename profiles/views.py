f06da726aae4ffccdfd76a8d68fdcd03417addfb
Francisco Barrera <francisco.barrera@uptc.edu.co>
2017-07-22 23:15:33 -0500

    Refactor landing, problema con timezone

--- profiles/views.py
+++ profiles/views.py
@@ -17,7 +17,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.models import User
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
-from datetime import datetime, timezone
+from datetime import datetime#, timezone
+from django.utils import timezone
 # Create your views here.
 
 
