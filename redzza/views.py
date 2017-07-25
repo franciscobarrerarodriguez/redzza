6fa559111604b2d440aabaad3a62b098ad7fe1d8
larry852 <larryportocarrero@gmail.com>
2017-07-25 11:44:13 -0500

    Redireccion desde index a admin, si existe sesion de administrador

--- redzza/views.py
+++ redzza/views.py
@@ -13,7 +13,10 @@ from django.shortcuts import render, redirect
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect('dashboard')
+        if request.user.is_staff:
+            return redirect('admin')
+        else:
+            return redirect('dashboard')
     return render(request, 'landing.html')
 
 
