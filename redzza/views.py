16c9a4a61b33420de0088ba9494e2ff1356bf96c
diegosierra01 <diego.sierra01@uptc.edu.co>
2017-07-28 18:58:42 -0500

    metodos para borar ficheros cuando se borran registros

--- redzza/views.py
+++ redzza/views.py
@@ -32,10 +32,10 @@ def queries(request):
     # query = Notice.create(get_object_or_404(Profile, gender='F'), Category.objects.get(id=60), "carro renault 4", "mi carrito de siempre", 1)
     # query = Notice.searchTitle("arro", Place.searchCity(3))
     notice = get_object_or_404(Notice, title="carro renault 4")
-    # query = Image.create(notice, "/home/diego/Documents/redzza/redzza/media/avatars/linea.png")
-    query = Video.create(notice, "/home/diego/Documents/redzza/redzza/media/videos/Gorillaz.mp4")
+    query = Image.create(notice, "/home/diego/2.jpg")
+    # query = Video.create(notice, "/home/diego/Documents/redzza/redzza/media/videos/Gorillaz.mp4")
     # para acceder a la url de la imagen se accede a Image.image.name
-    return HttpResponse(query.video.name)
+    return HttpResponse(query.image.name)
     # type -> tipo de campo
     # dir atributos de la clase
     pass
--- things/models.py
+++ things/models.py
@@ -6,6 +6,8 @@ from categories.models import Category
 from datetime import datetime
 from django.core.validators import validate_comma_separated_integer_list
 from django.core.files import File
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 # Create your models here.
 
 class Notice(models.Model):
@@ -141,6 +143,7 @@ class Service(models.Model):
 
 # https://openwebinars.net/blog/tutorial-django-modelos-bbdd-donde-guardar-informacion/
 
+
 class Image(models.Model):
     notice = models.ForeignKey(Notice)
     image = models.ImageField(upload_to='productos')
@@ -150,13 +153,20 @@ class Image(models.Model):
         # se abre el archivo local en binario
         f = open(pathimage, 'rb')
         # se guarda el archivo con extension
-        image.image.save(notice.title + '.jpg', File(f))
+        image.image.save(notice.title, File(f))
         return image
 
     def createGoogle(notice, pathimage):
         image = Image(notice=notice, image=pathimage)
         image.save()
         return image
+
+
+# metodo para borrar archivos cuando se borre el registro
+@receiver(post_delete, sender=Image)
+def photo_delete(sender, instance, **kwargs):
+    """ Borra los ficheros de las fotos que se eliminan. """
+    instance.image.delete(False)
 
 
 class Video(models.Model):
@@ -177,6 +187,12 @@ class Video(models.Model):
         video = Video(notice=notice, video=url)
         video.save()
         return video
+
+# metodo para borrar archivos cuando se borre el registro
+@receiver(post_delete, sender=Video)
+def video_delete(sender, instance, **kwargs):
+    """ Borra los ficheros de los videos que se eliminan. """
+    instance.video.delete(False)
 
 
 class Commentary(models.Model):
