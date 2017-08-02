4d7cde3710d4b544f5134aa5ec85a5d267e15689
larry852 <larryportocarrero@gmail.com>
2017-08-02 11:47:25 -0500

    Eliminacion de rutas estaticos

--- redzza/urls.py
+++ redzza/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.static import serve
 from . import views
-from . import settings
 
 urlpatterns = [
     # URL para comprobar el funcionamiento de las consultas a la base de datos
@@ -18,6 +16,4 @@ urlpatterns = [
     # Urls - referente a usuarios y sus perfiles
     # ---- Debe estar de ultimas - url de usuario -----
     url(r'^', include('profiles.urls')),
-    # staticfiles
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
