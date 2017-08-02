0e2cf8883b14a1fe13246f04a816ee7885bdd12d
larry852 <larryportocarrero@gmail.com>
2017-08-02 11:45:50 -0500

    Eliminacion de importacion de include

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from . import views
