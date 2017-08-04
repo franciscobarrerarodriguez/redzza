6da2087fa45939937b7a7d6fd3c18dc5bbd97e1a
larry852 <larryportocarrero@gmail.com>
2017-08-04 10:06:50 -0500

    Configurado api para profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
+from .models import Profile
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
+        model = Profile
+        fields = ['id']
--- redzza/urls.py
+++ redzza/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     # Url - Administracion
     url(r'^admin/', admin.site.urls,),
     # Urls - referente a usuarios y sus perfiles
-    url(r'^', include('profiles.urls')),
+    url(r'^api/v1/', include('profiles.urls')),
     # Urls - referente a categorias
-    url(r'^', include('categories.urls')),
+    url(r'^api/v1/', include('categories.urls')),
     # Urls - referente a cosas
-    url(r'^', include('things.urls')),
+    url(r'^api/v1/', include('things.urls')),
     # rest_framework
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
