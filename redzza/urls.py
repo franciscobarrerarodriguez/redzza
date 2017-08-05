d7c1d02f939dcffe205fb81f273044f8fca86a42
larry852 <larryportocarrero@gmail.com>
2017-08-04 19:52:43 -0500

    Url Admin

--- redzza/urls.py
+++ redzza/urls.py
@@ -16,7 +16,7 @@ router.registry.extend(things_router.registry)
 router.registry.extend(tags_router.registry)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls,),
+    url(r'^', admin.site.urls,),
     url(r'^api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest-auth/', include('rest_auth.urls')),
