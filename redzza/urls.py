8c0b228df6616ff2e7aef5e3b82528e3c36221be
larry852 <larryportocarrero@gmail.com>
2017-08-07 17:56:15 -0500

    Generacion de token nuevo al momento de login

--- redzza/urls.py
+++ redzza/urls.py
@@ -8,6 +8,7 @@ from profiles.urls import router as profiles_router
 from categories.urls import router as categories_router
 from things.urls import router as things_router
 from tags.urls import router as tags_router
+from rest_framework_expiring_authtoken import views
 
 router = routers.DefaultRouter()
 router.registry.extend(profiles_router.registry)
@@ -20,6 +21,7 @@ urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/login/', views.obtain_expiring_auth_token),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
