2ce3d0ef95db9cd3b2aabfafb80d65171f6d72e4
larry852 <larryportocarrero@gmail.com>
2017-08-17 12:04:54 -0500

    Agregado de /

--- redzza/adapter.py
+++ redzza/adapter.py
@@ -7,6 +7,6 @@ class DefaultAccountAdapterCustom(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
         current_site = Site.objects.get_current()
         context['activate_url'] = 'http://%s/' % (current_site.domain) + \
-            'verify-email/' + context['key']
+            'verify-email/' + context['key'] + '/'
         msg = self.render_mail(template_prefix, email, context)
         msg.send()
