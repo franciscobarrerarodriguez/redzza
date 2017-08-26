6d107315fdae6e8796f04d6f970d4c14a7db3e4d
larry852 <larryportocarrero@gmail.com>
2017-08-26 17:18:32 -0500

    Site para frontend

--- redzza/adapter.py
+++ redzza/adapter.py
@@ -1,11 +1,11 @@
 from allauth.account.adapter import DefaultAccountAdapter
-from .site import CURRENT_SITE
+from .site import CURRENT_SITE_FRONTEND
 
 
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
 
     def send_mail(self, template_prefix, email, context):
-        context['activate_url'] = CURRENT_SITE + \
+        context['activate_url'] = CURRENT_SITE_FRONTEND + \
             '/verify-email/' + context['key'] + '/'
         msg = self.render_mail(template_prefix, email, context)
         msg.send()
--- redzza/site.py
+++ redzza/site.py
@@ -1,3 +1,4 @@
 from django.contrib.sites.models import Site
 
 CURRENT_SITE = 'http://%s' % (Site.objects.get(id=3).domain)
+CURRENT_SITE_FRONTEND = 'http://%s' % (Site.objects.get(id=2).domain)
