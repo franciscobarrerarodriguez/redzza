c148c5cecc282d3577c9fb3aa59193a55ac2c79b
larry852 <larryportocarrero@gmail.com>
2017-09-05 14:47:19 -0500

    Cambio de direccion de envio de emails

--- redzza/settings.py
+++ redzza/settings.py
@@ -63,8 +63,8 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "larryportocarrero@gmail.com"
-EMAIL_HOST_PASSWORD = "dqggjrqpjndawkhg"
+EMAIL_HOST_USER = "redzzaservice@gmail.com"
+EMAIL_HOST_PASSWORD = "redzza852"
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
