4d5ddc764b895ebbf4e56d0e866f44993a4ad3d5
larry852 <larryportocarrero@gmail.com>
2017-09-14 23:01:39 -0500

    Cambio de configuracion site

--- redzza/site.py
+++ redzza/site.py
@@ -1,8 +1,13 @@
 from django.contrib.sites.models import Site
+from .settings import DEBUG
 
 try:
-    CURRENT_SITE = 'http://%s' % (Site.objects.get(id=5).domain)
-    CURRENT_SITE_FRONTEND = 'http://%s' % (Site.objects.get(id=6).domain)
+    if DEBUG:
+        CURRENT_SITE = 'http://%s' % (Site.objects.get(id=5).domain)
+        CURRENT_SITE_FRONTEND = 'http://%s' % (Site.objects.get(id=6).domain)
+    else:
+        CURRENT_SITE = 'http://%s' % (Site.objects.get(id=5).domain)
+        CURRENT_SITE_FRONTEND = 'http://%s' % (Site.objects.get(id=7).domain)
 except Exception as e:
     CURRENT_SITE = 'http://%s' % ('redzza.com')
     CURRENT_SITE_FRONTEND = 'http://%s' % ('redzza.com')
