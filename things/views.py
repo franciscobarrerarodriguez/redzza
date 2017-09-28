2a6f810ad58c44d571e2ab66b7cee08762c9c3e8
larry852 <larryportocarrero@gmail.com>
2017-09-28 16:03:13 -0500

    Creacion de comentario con token

--- things/views.py
+++ things/views.py
@@ -102,6 +102,15 @@ class CommentaryViewSet(viewsets.ModelViewSet):
     queryset = Commentary.objects.all()
     serializer_class = CommentarySerializer
 
+    def create(self, request, *args, **kwargs):
+        profile = viewsProfiles.getProfile(request.user)
+        request.data['profile'] = profile.id
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         if request.user != instance.profile.user:
