52377687a273eb72f9959a0e38bba6a06c5ddb2d
larry852 <larryportocarrero@gmail.com>
2017-09-28 16:26:58 -0500

    Creacion de video bajo token

--- things/views.py
+++ things/views.py
@@ -79,7 +79,6 @@ class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
 
     def create(self, request, *args, **kwargs):
-        print(request.data['notice'])
         notice = Notice.getNotice(request.data['notice'])
         if request.user != notice.profile.user:
             return Response({'success': False, 'err': 'user-unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
@@ -100,6 +99,16 @@ class ImageViewSet(viewsets.ModelViewSet):
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+    def create(self, request, *args, **kwargs):
+        notice = Notice.getNotice(request.data['notice'])
+        if request.user != notice.profile.user:
+            return Response({'success': False, 'err': 'user-unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
