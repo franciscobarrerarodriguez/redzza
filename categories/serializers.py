286d69d9b73f7de85a57bd9a06a02105e383151e
larry852 <larryportocarrero@gmail.com>
2017-10-10 20:53:03 -0500

    Imagen sobre serializador en categoria

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from .models import Category, WantedCategory, SuggestedCategory
+from redzza.site import S3
+from redzza.settings import MEDIA_URL
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
+        image = serializers.SerializerMethodField()
+
+        def get_image(self, obj):
+            return S3 + MEDIA_URL + str(obj.image)
+
         model = Category
-        fields = ('id', 'pattern', 'name', 'description')
+        fields = ('id', 'pattern', 'name', 'description', 'image')
 
 
 class WantedCategorySerializer(serializers.HyperlinkedModelSerializer):
