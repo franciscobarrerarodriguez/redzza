888fef4d9235d5d617a87b5b35e7dc2f3db2d2ae
larry852 <larryportocarrero@gmail.com>
2017-11-09 17:24:55 -0500

    Visualizacion del color en admin

--- categories/admin.py
+++ categories/admin.py
@@ -6,7 +6,13 @@ from .models import Category, WantedCategory, SuggestedCategory
 
 @admin.register(Category)
 class AdminCategory(admin.ModelAdmin):
-    list_display = ('pattern', 'name')
+    def get_color_html(self, obj):
+        return """<div style="width: 25px; height: 25px; background: {};"> <div>""".format(obj.color)
+
+    get_color_html.short_description = 'Color'
+    get_color_html.allow_tags = True
+
+    list_display = ('pattern', 'name', 'get_color_html')
 
 
 @admin.register(WantedCategory)
